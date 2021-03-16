6a96184e6fa9a02ac237fda86a44c7560fd53283
Melwyn Francis Carlo <66683108+melwyncarlo@users.noreply.github.com>
2021-03-16 21:56:53 +0530

    Create INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# For the command terminal, copy-paste FROM here
+
+mkdir ~/.FreeCAD/Macro/AeroFoil_UI_Files
+wget --no-check-certificate -P ~/.FreeCAD/Macro https://raw.githubusercontent.com/melwyncarlo/MultiCopy/main/MultiCopy.FCMacro
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/MultiCopy_UI_Files/MultiCopy_Main_Dialog.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/MultiCopy_UI_Files/MultiCopy_Commands_Dialog.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/MultiCopy_UI_Files/mc_d_imgs.gif
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/MultiCopy_UI_Files/MultiCopy.svg
+
+# For the command terminal, copy-paste TO here
+
