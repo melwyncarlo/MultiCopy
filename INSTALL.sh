8b0a2970728841d152921cc3eeeeb238334ca060
melwyncarlo <melwyncarlo@gmail.com>
2021-03-26 16:10:53 +0530

    Updated INSTALL and ZIP files

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,12 +2,17 @@
 
 # For the command terminal, copy-paste FROM here
 
-mkdir ~/.FreeCAD/Macro/AeroFoil_UI_Files
+mkdir ~/.FreeCAD/Macro/MultiCopy
+mkdir ~/.FreeCAD/Macro/MultiCopy/resources
 wget --no-check-certificate -P ~/.FreeCAD/Macro https://raw.githubusercontent.com/melwyncarlo/MultiCopy/main/MultiCopy.FCMacro
-wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/MultiCopy_UI_Files/MultiCopy_Main_Dialog.ui
-wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/MultiCopy_UI_Files/MultiCopy_Commands_Dialog.ui
-wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/MultiCopy_UI_Files/mc_d_imgs.gif
-wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/MultiCopy_UI_Files/MultiCopy.svg
+wget --no-check-certificate -P ~/.FreeCAD/Macro https://raw.githubusercontent.com/melwyncarlo/MultiCopy/main/MultiCopy.svg
+wget --no-check-certificate -P ~/.FreeCAD/Macro/MultiCopy/resources https://raw.githubusercontent.com/melwyncarlo/MultiCopy/main/MultiCopy/resources/MultiCopy_Main_Dialog.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/MultiCopy/resources https://raw.githubusercontent.com/melwyncarlo/MultiCopy/main/MultiCopy/resources/MultiCopy_Commands_Dialog.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/MultiCopy/resources https://raw.githubusercontent.com/melwyncarlo/MultiCopy/main/MultiCopy/resources/MultiCopy.svg
+wget --no-check-certificate -P ~/.FreeCAD/Macro/MultiCopy https://raw.githubusercontent.com/melwyncarlo/MultiCopy/main/MultiCopy/__init__.py
+wget --no-check-certificate -P ~/.FreeCAD/Macro/MultiCopy https://raw.githubusercontent.com/melwyncarlo/MultiCopy/main/MultiCopy/MultiCopyAuxFunc.py
+get --no-check-certificate -P ~/.FreeCAD/Macro/MultiCopy https://raw.githubusercontent.com/melwyncarlo/MultiCopy/main/MultiCopy/MultiCopyGui.py
+get --no-check-certificate -P ~/.FreeCAD/Macro/MultiCopy https://raw.githubusercontent.com/melwyncarlo/MultiCopy/main/MultiCopy/MultiCopyCore.py
 
 # For the command terminal, copy-paste TO here
 
